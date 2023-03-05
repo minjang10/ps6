8a33a7ade316a50f83bdeb6bcb5e570d2a3ec479
minjang10 <minjang516@gmail.com>
2023-03-05 01:29:27 -0800

    added plot

--- ps6/app.R
+++ ps6/app.R
@@ -19,10 +19,11 @@ ui <- fluidPage(
     tabsetPanel(
       tabPanel(
         "About",
-        p("This dataset shows many different data points of people with Diabetes."),
+        p("This dataset shows many different data points of people with", strong("Diabetes.")),
         p(""),
-        p("The diabetes column is either 0, 1, or 2. 0 for no diabetes, 1 for type 1, and 2 for type 2 diabetes."),
-        p("Here are 6 random samples from the data: "),
+        p("The diabetes column is either 0, 1, or 2.", em("0"), "for no diabetes,",
+          em("1"), "for type 1, and", em("2"), "for type 2 diabetes."),
+        p("Here are", em("6 random samples"), "from the data: "),
         tableOutput("sample")
       ),
       tabPanel(
@@ -33,12 +34,26 @@ ui <- fluidPage(
               "bins",
               "Number of Bins: ",
               min = 1,
-              max = 50,
-              value = 25
+              max = 15,
+              value = 10
+            ),
+            radioButtons(
+              "color",
+              "Choose a bin color",
+              c(
+                "red",
+                "blue",
+                "green",
+                "orange",
+                "yellow",
+                "purple",
+                "black"
+              )
             )
           ),
           mainPanel(
-            plotOutput("distPlot")
+            plotOutput("distPlot"),
+            textOutput("text")
           )
         )   
       ),
@@ -48,16 +63,27 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+    diab <- read_csv("diabetes.csv")
 
     output$distPlot <- renderPlot({
-      diab <- read_csv("diabetes.csv")
       ggplot(data = diab) +
-        geom_histogram(mapping = aes(x = income)) +
-        facet_wrap(~Diabetes_012)
+        geom_histogram(mapping = aes(x = Income), bins = input$bins, fill = input$color) +
+        facet_wrap(~Diabetes_012) +
+        xlab("income (0-8)") +
+        ylab("number of people") +
+        ggtitle("Income versus number of diabetes patients")
+    })
+    
+    output$text <- renderText({
+      n0 <- nrow(diab[diab$Diabetes_012 == 0 && !is.na(diab$Income),])
+      n1 <- nrow(diab[diab$Diabetes_012 == 1 && !is.na(diab$Income),])
+      n2 <- nrow(diab[diab$Diabetes_012 == 2 && !is.na(diab$Income),])
+      avg0 <- sum(diab[!is.na(diab$Income),])
+      paste("The average income")
     })
     
     output$sample <- renderTable({
-      diab <- read_csv("diabetes.csv")
       sample_n(diab, 6)
     })
 }
